9f05ac524a1c6a4ee89d47d1587dd1cd7ab6fdfb
als8921 <als8921@naver.com>
2024-11-11 16:45:34 +0900

    add : lift_control 패키지 생성

---
+++ src/lift_control/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+package_name = 'lift_control'
+submodules = 'lift_control/submodules'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name, submodules],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='lmc',
+    maintainer_email='als8921@naver.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
