94e7dde1b01f80550f1792e992cf0e3a9f245fca
als8921 <als8921@naver.com>
2024-11-19 15:03:54 +0900

    rename : launch 파일 이름 변경

--- src/mobile_control/launch/position_control.launch.py
+++ src/mobile_control/launch/position_control.launch.py
@@ -3,8 +3,6 @@ from launch_ros.actions import Node
 
 def generate_launch_description():
     ld = LaunchDescription()
-
-    ld.add_action(Node(package="mobile_control", executable="twist_transform"))
     ld.add_action(Node(package="mobile_control", executable="control"))
     ld.add_action(Node(package="mobile_control", executable="command"))
 
