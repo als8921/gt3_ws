01b6b681fac7f0ce2227925a97a7e583561952d7
als8921 <als8921@naver.com>
2024-11-20 09:27:57 +0900

    refactor : Enum을 사용하여 State 구분, 코드 정리

--- src/mobile_control/mobile_control/MobileController.py
+++ src/mobile_control/mobile_control/MobileController.py
@@ -2,12 +2,14 @@ import rclpy
 from rclpy.node import Node
 from nav_msgs.msg import Odometry
 from yhs_can_interfaces.msg import CtrlCmd
-from std_msgs.msg import Float32MultiArray  # Float32MultiArray 메시지 추가
+from std_msgs.msg import Float32MultiArray
 from rclpy.qos import qos_profile_sensor_data
 import tf_transformations
 import math
 import time
+from enum import Enum
 
+### Parameters ###
 LinearKp = 0.73
 AngularKp = 1.5
 
@@ -16,6 +18,7 @@ MaxLinearSpeed = 0.5        # [m/s]
 MaxAngularSpeed = 50        # [deg/s]
 
 ThetaErrorBoundary = 0.1    # 각도 명령 허용 오차
+####################
 
 def NormalizeAngle(angle):
     return (angle + 180) % 360 - 180
@@ -25,6 +28,12 @@ def AngularSpeedLimit(speed, maxSpeed = MaxAngularSpeed):
 
 def LinearSpeedLimit(speed, minSpeed = MinLinearSpeed, maxSpeed = MaxLinearSpeed):
     return float(max(minSpeed, min(speed, maxSpeed)))
+
+class State(Enum):
+    Stop = "Stop"
+    InitialRotate = 0
+    MoveForward = 1
+    FinalRotate = 2
 
 class Position:
     def __init__(self):
@@ -39,34 +48,23 @@ class Position:
     def RelativeAngle(self, other):
         return NormalizeAngle(math.degrees(math.atan2(other.y - self.y, other.x - self.x)))
     
-    
-    
-
 class ControlNode(Node):
     def __init__(self):
-        super().__init__('control_node')
+        super().__init__('mobile_control')
         self.create_subscription(Odometry, '/odom', self.odom_cb, qos_profile=qos_profile_sensor_data)
         self.create_subscription(Float32MultiArray, '/target', self.command_cb, 10)  # 목표 위치 및 자세 구독
         self.pub_command = self.create_publisher(CtrlCmd, 'ctrl_cmd', 10)
 
+        self.timer = self.create_timer(1.0 / 30.0, self.timer_callback)  # 30Hz
+
+        ### Position 객체 ###
         self.Pos = Position()       # Robot의 Odom 위치
         self.CmdPos = Position()    # 목표 위치
         self.StartPos = Position()  # 전진 명령을 시작할 때의 위치
 
-        # 최대 속도 및 각속도 설정
-        self.maxlinear_speed = 0.3      # 최대 선속도 (m/s)
-        self.maxangular_speed = 30      # 최대 각속도 (deg/s)
-
-        # 현재 선속도 변수 초기화
         self.current_linear_speed = 0.0
-
-
-        # 상태 변수 추가
-        self.state = 'idle'  # 초기 상태를 idle로 설정
-
-        # 이동 거리 및 시작 위치 변수
         self.target_distance = 0.0
-        self.timer = self.create_timer(1.0 / 30.0, self.timer_callback)  # 30Hz
+        self.state = State.Stop
 
 
     def odom_cb(self, msg):
@@ -87,15 +85,15 @@ class ControlNode(Node):
 
             self.get_logger().info(f'Target received: Position: {[self.CmdPos.x, self.CmdPos.y]}, Theta: {self.CmdPos.theta}')
             if(math.sqrt((self.CmdPos.x - self.Pos.x) ** 2  + (self.CmdPos.y - self.Pos.y) ** 2) < 0.1):
-                self.state = 'rotate_to_final_theta'
+                self.state = State.FinalRotate
             else:
-                self.state = 'rotate_to_target'
+                self.state = State.InitialRotate
 
     def timer_callback(self):
-        if self.state == 'rotate_to_target':
+        if self.state == State.InitialRotate:
             self.Rotate(self.Pos.RelativeAngle(self.CmdPos))
             if abs(self.Pos.RelativeAngle(self.CmdPos) - self.Pos.theta) <= ThetaErrorBoundary:
-                self.state = 'move_forward'
+                self.state = State.MoveForward
                 self.StartPos.x = self.Pos.x
                 self.StartPos.y = self.Pos.y
                 self.target_distance = Position.RelativeDistance(self.CmdPos, self.StartPos)
@@ -103,7 +101,7 @@ class ControlNode(Node):
                 self.PublishCtrlCmd(0, 0)  # 최종적으로 속도 0으로 설정
                 time.sleep(0.5)
 
-        elif self.state == 'move_forward':
+        elif self.state == State.MoveForward:
             current_distance = Position.RelativeDistance(self.Pos, self.StartPos)
             _error = self.target_distance - current_distance
             
@@ -111,7 +109,7 @@ class ControlNode(Node):
                 self.current_linear_speed = 0
                 self.PublishCtrlCmd(0, 0)  # 최종적으로 속도 0으로 설정
                 self.get_logger().info(f'MoveForward Finish {self.target_distance:.2f}[m]')
-                self.state = 'rotate_to_final_theta'
+                self.state = State.FinalRotate
                 time.sleep(0.5)
             else:
                 # 목표 선속도 계산 (최대 선속도로 제한)
@@ -129,11 +127,11 @@ class ControlNode(Node):
                 self.PublishCtrlCmd(linear_speed=LinearSpeedLimit(self.current_linear_speed))
 
 
-        elif self.state == 'rotate_to_final_theta':
+        elif self.state == State.FinalRotate:
             self.Rotate(NormalizeAngle(self.CmdPos.theta))
             if abs(NormalizeAngle(self.CmdPos.theta - self.Pos.theta)) <= ThetaErrorBoundary:
                 self.PublishCtrlCmd(0, 0)  # 최종적으로 속도 0으로 설정
-                self.state = 'finished'
+                self.state = State.Stop
                 self.get_logger().info('목표 위치 도달.')
                 self.get_logger().info(f'목표 위치 : {self.CmdPos.x, self.CmdPos.y, self.CmdPos.theta}')
                 self.get_logger().info(f'현재 위치 : {self.Pos.x, self.Pos.y, self.Pos.theta}')
@@ -155,8 +153,8 @@ class ControlNode(Node):
         ctrl_cmd.ctrl_cmd_x_linear = -LinearSpeedLimit(linear_speed)
         ctrl_cmd.ctrl_cmd_y_linear = 0.0
         ctrl_cmd.ctrl_cmd_z_angular = AngularSpeedLimit(angular_speed)
-        self.get_logger().info(f'POS: {[self.Pos.x, self.Pos.y]},  {self.Pos.theta:.2f}[deg]')
-        self.get_logger().info(f'Linear Speed: {linear_speed:.2f}[m/s], Angular Speed: {ctrl_cmd.ctrl_cmd_z_angular:.2f}[deg/s]')
+        self.get_logger().info(f'{(self.Pos.x, self.Pos.y)}[m], {self.Pos.theta:.2f}[deg]')
+        self.get_logger().info(f'{linear_speed:.2f}[m/s], {ctrl_cmd.ctrl_cmd_z_angular:.2f}[deg/s]')
         self.pub_command.publish(ctrl_cmd)
 
 def main(args=None):
