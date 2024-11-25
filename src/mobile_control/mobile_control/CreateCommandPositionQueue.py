f67eae39ab00ff7ad81c106997170537575a43d4
als8921 <als8921@naver.com>
2024-11-25 14:19:36 +0900

    rename : 테스트 코드 파일명 변경

--- src/mobile_control/mobile_control/CreateCommandPositionQueue.py
+++ src/mobile_control/mobile_control/CreateCommandPositionQueue.py
@@ -18,18 +18,18 @@ def CreateCommandPositionQueue(x1, y1, x2, y2, D_horizontal, D_vertical, D_task)
     # theta 계산 (외적 방향의 각도)
     theta_degrees = np.degrees(np.arctan2(-direction_vector[1], -direction_vector[0]))
 
-    queue = deque()
+    position_queue = deque()
     
     for i in range(0, int((move_d - 2 * D_horizontal) // D_task) + 1):
         current_position = new_pos + move_vector_norm * (D_task * i)
-        queue.append((current_position[0], current_position[1], theta_degrees))
+        position_queue.append((current_position[0], current_position[1], theta_degrees))
 
     # 마지막 위치 추가 (모듈로 연산으로 인한 위치)
     if (move_d - 2 * D_horizontal) % D_task != 0:
         last_position = new_pos + move_vector_norm * (move_d - 2 * D_horizontal)
-        queue.append((last_position[0], last_position[1], theta_degrees))
+        position_queue.append((last_position[0], last_position[1], theta_degrees))
     
-    return queue
+    return position_queue
 
 # 사용 예시
 x1, y1 = 1, 0
@@ -38,9 +38,9 @@ D_horizontal = 1  # x 방향 거리
 D_vertical = 0.5  # 외적 방향 거리
 D_task = 1.2  # 작업 거리
 
-queue = CreateCommandPositionQueue(x1, y1, x2, y2, D_horizontal, D_vertical, D_task)
+position_queue = CreateCommandPositionQueue(x1, y1, x2, y2, D_horizontal, D_vertical, D_task)
 
 # 큐 내용 출력
 print("새로운 위치 (x, y, theta):")
-for position in queue:
+for position in position_queue:
     print(position)
