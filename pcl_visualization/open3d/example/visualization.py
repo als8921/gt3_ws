7754013f2b8e5a29fc9e25b7792dc1756e99af72
als8921 <als8921@naver.com>
2024-12-18 10:55:07 +0900

    feat : DownSampling, Normal Vector 생성

--- pcl_visualization/open3d/example/visualization.py
+++ pcl_visualization/open3d/example/visualization.py
@@ -1,21 +1,31 @@
 import open3d as o3d
 import numpy as np
 
-# 파일 경로
-file_path = 'pcl_visualization/pointcloud_data/pointcloud_data_0.txt'
+pcl_path = 'pcl_visualization/pointcloud_data/pointcloud_data_0.txt'
 
-# 데이터를 읽어오는 함수
-def load_point_cloud(file_path):
-    # 파일에서 데이터를 읽어오기
-    data = np.loadtxt(file_path)
+def load_point_cloud(pcl_path):
+    data = np.loadtxt(pcl_path)
     return data
 
-# 포인트 클라우드 데이터 로드
-points = load_point_cloud(file_path)
+points = load_point_cloud(pcl_path)
 
 # Open3D 포인트 클라우드 객체 생성
 pcd = o3d.geometry.PointCloud()
 pcd.points = o3d.utility.Vector3dVector(points)
 
-# 포인트 클라우드 시각화
-o3d.visualization.draw_geometries([pcd])
+# Voxel Filtter
+downpcd = pcd.voxel_down_sample(voxel_size = 0.01)
+
+# Normal 벡터 추정
+downpcd.estimate_normals(
+    search_param=o3d.geometry.KDTreeSearchParamHybrid(radius=0.15, max_nn=30)
+)
+
+if(downpcd.has_normals()):
+    print("Normal Vector 가 있음")
+else:
+    print("Normal Vector 가 없음")
+
+
+# Open3D 시각화
+o3d.visualization.draw_geometries([downpcd], point_show_normal=True)
