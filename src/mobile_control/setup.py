6d65828939434d5a75c737fd3bc1357e9751d8b4
als8921 <als8921@naver.com>
2025-01-06 11:21:01 +0900

    remove : 필요 없는 파일, Submodule 폴더 제거

--- src/mobile_control/setup.py
+++ src/mobile_control/setup.py
@@ -3,12 +3,11 @@ import os
 from glob import glob
 
 package_name = 'mobile_control'
-submodules = 'mobile_control/submodules'
 
 setup(
     name=package_name,
     version='0.0.0',
-    packages=[package_name, submodules],
+    packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
