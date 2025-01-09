70b767c22338383d0d2584ea3f435ad583068404
als8921 <als8921@naver.com>
2025-01-09 16:28:12 +0900

    chore : Setting 파일 이름 변경

--- jsonIO/test.py
+++ jsonIO/test.py
@@ -1,5 +1,5 @@
-import jsonManager
+import SettingJson
 
-print(jsonManager.load_settings())
-print(jsonManager.update_settings(0, 0))
-print(jsonManager.load_settings())
+print(SettingJson.load_settings())
+print(SettingJson.update_settings(0, 0))
+print(SettingJson.load_settings())
