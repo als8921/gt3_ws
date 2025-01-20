fb6ba1abfc4ba22f0e34376d48a66d7890f7aa0a
als8921 <als8921@naver.com>
2025-01-20 15:50:51 +0900

    feat : 배터리 온도 Publish 기능 추가

--- src/mobile_control/mobile_control/BatteryCheck.py
+++ src/mobile_control/mobile_control/BatteryCheck.py
@@ -8,12 +8,14 @@ class BmsPublisher(Node):
     def __init__(self):
         super().__init__('battery_publisher')
         self.subscriber = self.create_subscription(ChassisInfoFb, '/chassis_info_fb', self.callback, 10)
-        self.publisher = self.create_publisher(Int32, '/mobile/battery', 10)
+        self.battery_pub = self.create_publisher(Int32, '/mobile/battery', 10)
+        self.temperature_pub = self.create_publisher(Int32, '/mobile/temperature', 10)
 
     def callback(self, msg):
-        bms_flag_value = int(msg.bms_flag_fb.bms_flag_fb_soc)
-        self.publisher.publish(Int32(data=bms_flag_value))
-        time.sleep(1)
+        battery_value = int(msg.bms_flag_fb.bms_flag_fb_soc)
+        battery_temperature = int(msg.bms_flag_fb.bms_flag_fb_hight_temperature)
+        self.battery_pub.publish(Int32(data=battery_value))
+        self.temperature_pub.publish(Int32(data=battery_temperature))
 
 def main(args=None):
     rclpy.init(args=args)
